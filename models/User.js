a03a633b9980695a7267819f15c84e748958a61f
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-11-22 18:42:37 -0800

    add data.js, seed.js, utils folder, and code to server.js and User.js

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const userSchema = require('./Thought');
+
+// 
+const userSchema = new Schema(
+  {
+    first: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    last: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    github: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    assignments: [userSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
